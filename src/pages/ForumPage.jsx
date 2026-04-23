94094d20708aada5cfc2202a17eaf1d56df2c2a2
equipetecNOva <equipetechniqueharibocompagnie@gmail.com>
2026-04-23 08:12:03 +0000

    Fix forum double navbar by removing duplicate PublicLayout wrapper

--- src/pages/ForumPage.jsx
+++ src/pages/ForumPage.jsx
@@ -1,15 +1,12 @@
-import PublicLayout from '@/components/layout/PublicLayout';
 import Forum from '@/components/forum/Forum';
 
 const ForumPage = () => {
   return (
-    <PublicLayout>
-      <div className="py-8 px-4">
-        <div className="max-w-6xl mx-auto">
-          <Forum />
-        </div>
+    <div className="py-8 px-4">
+      <div className="max-w-6xl mx-auto">
+        <Forum />
       </div>
-    </PublicLayout>
+    </div>
   );
 };
 
